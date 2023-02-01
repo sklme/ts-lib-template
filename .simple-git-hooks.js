d412a10567d59fa9753227d8feb6cb6abf841bfc
wrynnsun <wrynnsun@tencent.com>
2023-02-01 19:08:21 +0800

    chore: hooks

--- .simple-git-hooks.js
+++ .simple-git-hooks.js
@@ -5,9 +5,9 @@
 
 module.exports = {
   // 'prepare-commit-msg': 'exec < /dev/tty && npx cz --hook || true',
-  'pre-commit': 'npm run test',
+  'pre-push': 'npm run test',
   'commit-msg':
     'npx tsx scripts/verify_commit_msg.ts $1 && npx vue-tsc --noEmit',
   // 设置为true，表示在重新运行npx simple-git-hooks的时候，不要移除的hook
-  // preserveUnused: ['pre-push'],
+  // preserveUnused: ['pre-commit'],
 };
